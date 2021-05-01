5df4b9d208950d5e1dbf0339f4ee2374e9ae54fa
Schenker, Remo <remo.schenker@zuehlke.com>
2021-05-01 16:44:26 +0200

    Implement domain operation to update a team

---
+++ application/src/main/java/ch/resrc/tichu/domain/operations/UpdateTeam.java
@@ -0,0 +1,13 @@
+package ch.resrc.tichu.domain.operations;
+
+import ch.resrc.tichu.capabilities.errorhandling.Problem;
+import ch.resrc.tichu.domain.entities.Team;
+import ch.resrc.tichu.use_cases.ports.output_boundary.OutputBoundary;
+import io.vavr.collection.Set;
+import io.vavr.control.Either;
+
+@FunctionalInterface
+public interface UpdateTeam extends OutputBoundary {
+
+  Either<? extends Problem, Set<Team>> update(Set<Team> existing, Team updatedTeam);
+}
