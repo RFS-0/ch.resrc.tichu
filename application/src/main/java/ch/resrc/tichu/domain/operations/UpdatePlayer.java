8d4636e65dd23dd7793f10759c24be7e921ca8cf
Schenker, Remo <remo.schenker@zuehlke.com>
2021-05-02 11:02:33 +0200

    Implement domain operation to update player

---
+++ application/src/main/java/ch/resrc/tichu/domain/operations/UpdatePlayer.java
@@ -0,0 +1,13 @@
+package ch.resrc.tichu.domain.operations;
+
+import ch.resrc.tichu.capabilities.errorhandling.Problem;
+import ch.resrc.tichu.domain.entities.Player;
+import ch.resrc.tichu.use_cases.ports.output_boundary.OutputBoundary;
+import io.vavr.collection.Set;
+import io.vavr.control.Either;
+
+@FunctionalInterface
+public interface UpdatePlayer extends OutputBoundary {
+
+  Either<? extends Problem, Set<Player>> update(Set<Player> existing, Player updatedPlayer);
+}
